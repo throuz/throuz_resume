62b3824ec76a3d1aee455b803e763a47763c63d3
throuz <64568129+throuz@users.noreply.github.com>
2020-06-01 18:18:07 +0800

    Vcg: Update live demo

---
+++ precache-manifest.d070510eda78335effb12830726d64e9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d6b659292be94a2251",
+    "url": "/throuz_resume/css/app.a992830c.css"
+  },
+  {
+    "revision": "509e19dd13796405dd01",
+    "url": "/throuz_resume/css/chunk-vendors.3938e1d4.css"
+  },
+  {
+    "revision": "11fb86ff69ae103344fae3dec9fb727a",
+    "url": "/throuz_resume/icons/icon-128x128.png"
+  },
+  {
+    "revision": "6a65b081322d737ab2c510b4f1ee27ed",
+    "url": "/throuz_resume/icons/icon-144x144.png"
+  },
+  {
+    "revision": "aed6a769242d6f80580f9696d9cf4af0",
+    "url": "/throuz_resume/icons/icon-152x152.png"
+  },
+  {
+    "revision": "d1598a6021e47427be680106fe20f4e6",
+    "url": "/throuz_resume/icons/icon-192x192.png"
+  },
+  {
+    "revision": "3fa8d4b452a529b5ef21fb61c461803b",
+    "url": "/throuz_resume/icons/icon-384x384.png"
+  },
+  {
+    "revision": "9b33c37d169850335cf48ee7207ad617",
+    "url": "/throuz_resume/icons/icon-512x512.png"
+  },
+  {
+    "revision": "2860811cd62485fbf35e127e015db124",
+    "url": "/throuz_resume/icons/icon-72x72.png"
+  },
+  {
+    "revision": "76934af2edafcd70352ead2cdfcd7312",
+    "url": "/throuz_resume/icons/icon-96x96.png"
+  },
+  {
+    "revision": "b97d86fe2d71c5b6c70e8a40c2341fd1",
+    "url": "/throuz_resume/images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "3ee8176b415e7bf528cb0a8c7242fbd5",
+    "url": "/throuz_resume/images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "f94316b62243c6ee83a7f23b0747b271",
+    "url": "/throuz_resume/images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "e4c193837fc2de90c10a1e00227ed359",
+    "url": "/throuz_resume/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "b276b977ea22bc0737e8e9a1f4dc5626",
+    "url": "/throuz_resume/images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "4b51a4d991752878c7de7099d8d044e0",
+    "url": "/throuz_resume/images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "3b44e84d0a6d90428c1890330ea9c025",
+    "url": "/throuz_resume/images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "055d414e62a2242a2b3c53c435b68fa7",
+    "url": "/throuz_resume/images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "1a3fed79a66c35c6cc1a5d1d6ec026f0",
+    "url": "/throuz_resume/img/the_one.1a3fed79.png"
+  },
+  {
+    "revision": "0a3174bc86b9913eff5129750827b17d",
+    "url": "/throuz_resume/index.html"
+  },
+  {
+    "revision": "d9d6b659292be94a2251",
+    "url": "/throuz_resume/js/app.c5c1c749.js"
+  },
+  {
+    "revision": "509e19dd13796405dd01",
+    "url": "/throuz_resume/js/chunk-vendors.f3adc20c.js"
+  },
+  {
+    "revision": "765a1f81446aa26af6ed46bdfd810d95",
+    "url": "/throuz_resume/manifest.json"
+  },
+  {
+    "revision": "ac4311d48acac934f081f5bca807322b",
+    "url": "/throuz_resume/offline.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/throuz_resume/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,34 @@
-self.addEventListener('install', function (event) {
-    self.skipWaiting();
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-    var offlinePage = new Request('offline.html');
-    event.waitUntil(
-        fetch(offlinePage).then(function (response) {
-            return caches.open('offline2').then(function (cache) {
-                return cache.put(offlinePage, response);
-            });
-        }));
-}); self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        fetch(event.request).catch(function (error) {
-            return caches.open('offline2').then(function (cache) {
-                return cache.match('offline.html');
-            });
-        }));
-}); self.addEventListener('refreshOffline', function (response) {
-    return caches.open('offline2').then(function (cache) {
-        return cache.put(offlinePage, response);
-    });
-}); self.addEventListener('push', function (event) {
-    var data = event.data.json(); var opts = {
-        body: data.body,
-        icon: data.icon,
-        data: {
-            url: data.url
-        }
-    };
-    event.waitUntil(self.registration.showNotification(data.title, opts));
-}); self.addEventListener('notificationclick', function (event) {
-    var data = event.notification.data; event.notification.close(); event.waitUntil(
-        clients.openWindow(data.url)
-    );
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/throuz_resume/precache-manifest.d070510eda78335effb12830726d64e9.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "test"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
